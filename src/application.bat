fed4eb494a44f926496d8cd731e40ace2672da09
Allan Blanchard <a.blanchard@iia-laval.fr>
2020-01-22 15:26:36 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,5 +1,6 @@
 ### application.bat - 2020 - Blanchard Allan ###
+version = "2"
+build_hour = "11H00"
 
-echo "Copyright company - 2020"
-echo "Build : 10:00"
-echo "version 1.1"
+echo "Build ${build_hour}"
+echo "version ${version}"
